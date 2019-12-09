1245c87bb0855bdc1cb4657804f3a181288ae53e
nishiana524 <nishiana524@gmail.com>
2019-12-09 10:40:36 -0800

    with hash maps

--- src/wifi/Receiver.java
+++ src/wifi/Receiver.java
@@ -3,6 +3,7 @@
  
 
  
+import java.util.HashMap;
 import java.util.Queue;
 import java.io.PrintWriter;
 
@@ -15,6 +16,7 @@ import rf.RF;
 public class Receiver implements Runnable{
 	private ArrayBlockingQueue<Packet> DataQueue = new ArrayBlockingQueue<Packet>(1000);
 	//this will end up being a packet object
+	public static HashMap<Short, Short> seqNum;
 	
 	private RF theRF;
 	private Boolean[] ackQueue = new Boolean[4096];
@@ -52,6 +54,8 @@ public class Receiver implements Runnable{
 		this.theRF = theRF;
 		this.ackQueue = ackQueue;
 		this.output = output;
+		
+		seqNum = new HashMap<>();
 	}
 	
 	private void sendACK(Packet packet) { //sends an ack back to whoever send the packet to us
@@ -69,6 +73,37 @@ public class Receiver implements Runnable{
 	
 	private int toUs(byte[] packet) { //sees if the packet was ment for us
 		return (short) ((packet[2] << 8) | (packet[3] & 0xFF));
+		
+	}
+	
+	private boolean checkSeqNum(Packet p) {
+		boolean ordered = false;
+		int val = 0;
+		if(seqNum.containsKey(p.getScrAddr())) { // If MAC address already exists then check if the their sequence
+			//number is sequential to the their MAC address.
+			
+			val = seqNum.get(p.getScrAddr());
+			if ((val + 1) == p.getSequenceNum()) {
+				ordered = true;
+			}
+			else {
+				//if its not sequential
+				ordered = false;
+			}
+			//set the new sequence number
+			
+			
+		}
+		else {
+			
+			ordered = true;
+		}
+		//Get value, 
+		//check in  in
+		//seqNum.put(key, value)
+		seqNum.put(p.getScrAddr(), (short) p.getSequenceNum());
+		
+		return ordered;
 		
 	}
 	
@@ -92,6 +127,19 @@ public class Receiver implements Runnable{
 					else if(toUs(IncomingByteArray)==ourMAC){ //if the message was meant for specifically us
 						sendACK(IncomingPacket); //send ACK
 						DataQueue.add(IncomingPacket); //put packet into queue
+						
+						if(checkSeqNum(IncomingPacket) == false) {
+							output.print("THis is not ordered");
+							System.out.println("Here");
+						}
+						else {
+							output.print("Yay, I'm ordered! :D");
+							System.out.println("Here1");
+						}
+						
+						
+						//Check if the their MAC address was in the map
+						
 
 					}
 					else if(toUs(IncomingByteArray)==-1) { //if it was an open message to all who will listen
